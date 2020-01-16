6cca83637dfc565c54b5b00288baafe22c02bc98
Robin <robin.tschirschnitz@googlemail.com>
2020-01-16 16:19:02 +0100

    save the scrolling Position.

--- Snapcast/src/main/java/de/badaix/snapcast/AboutActivity.java
+++ Snapcast/src/main/java/de/badaix/snapcast/AboutActivity.java
@@ -21,27 +21,95 @@ package de.badaix.snapcast;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class AboutActivity extends AppCompatActivity {
 
+
+    // Calculate the % of scroll progress in the actual web page content
+    private float calculateProgression(WebView content) {
+        float positionTopView = content.getTop();
+        float contentHeight = content.getContentHeight();
+        float currentScrollPosition = content.getScrollY();
+        return (currentScrollPosition - positionTopView) / contentHeight;
+    }
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public Object onRetainCustomNonConfigurationInstance() {
+        WebView wv = findViewById(R.id.webView);
+        OrientationChangeData objectToSave = new OrientationChangeData();
+        objectToSave.mProgress = calculateProgression(wv);
+        return objectToSave;
+    }
+
+    // Container class used to save data during the orientation change
+    private final static class OrientationChangeData {
+        private float mProgress;
+    }
+
+    private boolean mHasToRestoreState = false;
+    private float mProgressToRestore;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_about);
+
+        try {
+            setContentView(R.layout.activity_about);
+        } catch (Exception e) {
+
+            Snackbar.make(findViewById(R.id.webView), this.getText(R.string.action_about_failed), Snackbar.LENGTH_LONG).show();
+        }
+
         try {
             getSupportActionBar().setTitle(getString(R.string.about) + " Snapcast");
-        } catch (Exception e) {
+        }catch (NullPointerException e) {
+            Snackbar.make(findViewById(R.id.webView), this.getText(R.string.action_nullpointer), Snackbar.LENGTH_LONG).show();
         }
         PackageInfo pInfo;
         try {
             pInfo = getPackageManager().getPackageInfo(getPackageName(), 0);
-            getSupportActionBar().setSubtitle("v" + pInfo.versionName);
+            try {
+                getSupportActionBar().setSubtitle("v" + pInfo.versionName);
+            }catch (NullPointerException e) {
+                Snackbar.make(findViewById(R.id.webView), this.getText(R.string.action_nullpointer), Snackbar.LENGTH_LONG).show();
+            }
         } catch (PackageManager.NameNotFoundException e) {
             e.printStackTrace();
         }
-        WebView wv = (WebView) findViewById(R.id.webView);
+        WebView wv = findViewById(R.id.webView);
+        wv.setWebViewClient(new MyWebViewClient());
         wv.loadUrl("file:///android_asset/" + this.getText(R.string.about_file));
+        OrientationChangeData data = (OrientationChangeData) getLastCustomNonConfigurationInstance();
+        if (data != null) {
+            mHasToRestoreState = true;
+            mProgressToRestore = data.mProgress;
+        }
     }
+
+    private class MyWebViewClient extends WebViewClient {
+        @Override
+        public void onPageFinished(WebView view, String url) {
+            if (mHasToRestoreState) {
+                mHasToRestoreState = false;
+                view.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        WebView wv = findViewById(R.id.webView);
+                        float webviewsize = wv.getContentHeight() - wv.getTop();
+                        if (webviewsize == 0){ wv.postDelayed(this, 10); return; }
+                        float positionInWV = webviewsize * mProgressToRestore;
+                        int positionY = Math.round(wv.getTop() + positionInWV);
+                        wv.scrollTo(0, positionY);
+                    }
+                    // Delay the scrollTo to make it work
+                });
+            }
+            super.onPageFinished(view, url);
+        }
+    }
+
 }
